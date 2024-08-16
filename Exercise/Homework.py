72ee8e51c641d734f99841295f6caad763790e62
VandaLeng <Vanda.Leng@student.passerellesnumeriques.org>
2024-08-16 18:00:59 +0700

    Add files via upload

---
+++ Exercise/Homework.py
@@ -0,0 +1,169 @@
+################1###################
+
+# text = "Vanda"
+# for char in text:
+#     print(char)
+
+# text = "Vanda"
+# for i in range(len(text)):
+#     print(text[i])
+     
+#############################2#########################
+# text = input("Enter your text: ")
+# count = 0
+# for char in text:
+#         count += 1
+# print(count)
+
+# text = input("Enter your text: ")
+# i = 0
+# for i in range(len(text)):
+#         i += 1
+# print(i)
+
+################3#######################
+# text = input("enter your text: ")
+# letter = ""
+# for char in text:
+#     if text.isupper(): 
+#        print("Yes")
+#     else:
+#        print("No")
+
+
+# text = input("Enters your letter: ")
+# result = "No"
+# for i in range(len(text)):
+#     if text[i].upper() == text[i] :
+#         result = "Yes"
+# print(result)
+
+
+
+# #######################4####################
+# number = input("Enter your text")
+# sum = 0
+# for i in range(len(number)):
+#      sum += int(number[i])
+# print(sum)
+    
+# text = "3 4 5 6" 
+# result = ""
+# for char in range (len(text)):
+#     if text[char]!=" ":
+#        result += text[char]
+# print(result)
+####################5###################
+
+# text = "4567809340985"
+# result = ""
+# for i in range(len(text)):
+#     if int(text[i]) % 2 == 1:
+#         result += text[i]
+# print(result)
+
+
+# text = "4567809340985"
+# number = 0
+# for i in range(len(text)):
+#     number += int(text[i])
+# print(number)
+
+# text = "4567809340985"
+# even_count = 0
+# for i in range(len(text)):
+#     if int(text[i]) % 2 == 0:
+#         even_count += int(text[i])
+# print(even_count)
+
+# text = input("enter your text: ")
+# sum = 0
+# for i in range(len(text)):
+#     if int(text[i]) % 2 == 0:
+#         sum += int(text[i])
+# print(sum)
+
+
+
+# text = "454639"
+# number = ""
+# sum = len(text)
+# for i in range(len(text)):
+#     number += text[sum - 1 - i]
+# print(number)
+
+
+##################6######################3
+
+# number=int(input("enter here:"))
+# if number % 2==1:
+#     print("Odd number")
+# else:
+#     print("even number")
+    
+###############7###################
+# num=int(input("enter number:"))
+# while num <= 20:
+#       print("continue")
+#       num=int(input("enter number:"))
+# print("Out of range")
+#################8####################
+
+# text="9394884039"
+# Count_8=0
+# for i in range (len(text)):
+#       if text[i]=="8":
+#             Count_8+=1
+# print(Count_8)
+
+
+
+# text="9394884039"
+# isFound_8 = False
+# index = 0
+# while not isFound_8:
+#     index += 1
+#     if text[index] == "8":
+#         isFound_8 = True
+#         result = index
+# print(result)
+
+#######################9####################
+# text = "3 4 5 6" 
+# result = ""
+# for char in range (len(text)):
+#     if text[char]!=" ":
+#        result += text[char]
+# print(result)
+
+# text = "3 4 5 6"
+# result = ""
+# for i in range (len(text)):
+#     if text[i]!=" ":
+#        result += str(int(text[i])*2) + " "
+# print(result)
+
+##################10###################
+
+# number = int(input("Enter number:"))
+# number_max = number
+# number_min = number
+# for i in range(5):
+#     number = int(input("Enter number:"))
+#     if number > number_max:
+#         number_max = number
+#     if number < number_min:
+#         number_min = number
+# print("Max =", number_max, "Min =", number_min)
+
+
+# max = 0
+# min = 0
+# for i in range(5):
+#     num = int(input("Enter number: "))
+#     if num > max:
+#         max = num
+#     if num < min:
+#         min = num
+# print("Max =", max)
+# print("Min =", min)
